e0c02d6ee5b0218cf61113259b47f48373bdd414
Djamil Elaidi <d-elaidi@ti.com>
2012-02-03 11:06:35 +0100

    OMAP4+: HSI: Aligned HSR FSM control onto CAWAKE detection
    HSR FSM enables the ACREADY to go to 1 and allows reception.
    With previous code, HSR FSM is enabled/disabled when clocks are enabled/disabled.
    But HSR FSM control shall be separated from HST state, and depend only on CAWAKE.
    
    Now HSR FSM is disabled/enabled when CAWAKE is low/high.
    
    Change-Id: I527bc0cc376218abd01d4e1c6cda2487ce0ecf27
    Signed-off-by: Djamil Elaidi <d-elaidi@ti.com>

--- drivers/omap_hsi/hsi_driver.c
+++ drivers/omap_hsi/hsi_driver.c
@@ -122,7 +122,6 @@ void hsi_save_ctx(struct hsi_dev *hsi_ctrl)
 		p->hst.arb_mode = hsi_inl(base, HSI_HST_ARBMODE_REG(port));
 
 		/* HSR */
-		p->hsr.mode = hsi_inl(base, HSI_HSR_MODE_REG(port));
 		if (!hsi_driver_device_is_hsi(pdev))
 			p->hsr.frame_size = hsi_inl(base,
 						HSI_HSR_FRAMESIZE_REG(port));
@@ -1141,9 +1140,6 @@ int hsi_runtime_resume(struct device *dev)
 	/* Restore context */
 	hsi_restore_ctx(hsi_ctrl);
 
-	/* Allow data reception */
-	hsi_hsr_resume(hsi_ctrl);
-
 	/* Restore HSR_MODE register value */
 	/* WARNING: works only in this configuration: */
 	/* - Flow = Synchronized */
@@ -1184,9 +1180,6 @@ int hsi_runtime_suspend(struct device *dev)
 
 	if (!hsi_ctrl->clock_enabled)
 		dev_warn(dev, "Warning: clock status mismatch vs runtime PM\n");
-
-	/* Forbid data reception */
-	hsi_hsr_suspend(hsi_ctrl);
 
 #ifdef CONFIG_MACH_TUNA
 	/* HSI is going to IDLE, it needs IO wakeup mechanism enabled */
