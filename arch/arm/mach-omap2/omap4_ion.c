1e23771efd2f9c6881a47bebda74f40e2c8fb5e3
Luden <luden@ghostmail.com>
2016-02-05 21:21:28 +0100

    OMAP4: tuna: adapt to dynamic secure_input heap allocation
    omap4_ion_heap_secure_input_addr needs a fixed value instead of
    a dynamically calculated one, which would change after overriding IPU_MEM_IOBUFS
    carveout size, causing problems as that address is hardcoded in our Ducati.
    
    Change-Id: Ic9aea856a4f6605061930dd377a9964eece3f8d7

--- arch/arm/mach-omap2/omap4_ion.c
+++ arch/arm/mach-omap2/omap4_ion.c
@@ -20,6 +20,7 @@
 
 /* Hardcoded Ducati heap address. */
 #define ESPRESSO_DUCATI_HEAP_ADDR 0xbae00000
+#define TUNA_DUCATI_HEAP_ADDR 0xba300000
 
 /*
  * Carveouts from higher end of RAM
@@ -141,8 +142,12 @@ void __init omap_ion_init(void)
 	/* carveout addresses */
 	omap4_smc_addr = PLAT_PHYS_OFFSET + omap_total_ram_size() -
 				omap4_smc_size;
+#ifdef CONFIG_MACH_TUNA /* fixed start address of ducati heap */
+	omap4_ion_heap_secure_input_addr = TUNA_DUCATI_HEAP_ADDR;
+#else
 	omap4_ion_heap_secure_input_addr = omap4_smc_addr -
 				omap4_ion_heap_secure_input_size;
+#endif
 	omap4_ion_heap_secure_output_wfdhdcp_addr =
 				omap4_ion_heap_secure_input_addr -
 				omap4_ion_heap_secure_output_wfdhdcp_size;
